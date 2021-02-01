0e4e93e80059e733be35aaa44b50743722efdc32
Wok <woctezuma@users.noreply.github.com>
2021-02-01 13:39:24 +0100

    Allow to manually set the resume value of the augmentation strength

--- train.py
+++ train.py
@@ -244,8 +244,8 @@ def setup_training_loop_kwargs(
 
     if p is not None:
         assert isinstance(p, float)
-        if aug != 'fixed':
-            raise UserError('--p can only be specified with --aug=fixed')
+        if resume != 'latest' and aug != 'fixed':
+            raise UserError('--p can only be specified with --resume=latest or --aug=fixed')
         if not 0 <= p <= 1:
             raise UserError('--p must be between 0 and 1')
         desc += f'-p{p:g}'
